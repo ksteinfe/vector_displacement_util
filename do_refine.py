b9d2f5d5838db507cab56985f24bc6d062c261b6
Kyle Steinfeld <ksteinfe@gmail.com>
2021-09-14 20:05:35 -0700

    ZDISP_INOUT and XYDISP_FULL

--- do_refine.py
+++ do_refine.py
@@ -3,9 +3,11 @@ import vdisp as vd
 PTH_SRC = r'C:\tmp'
 PTH_DST = r'C:\tmp'
 
+XYDISP_FULL = False # if True, xy displacements span the whole uv square (-1 to 1); if False, xy displacements span half the uv square (-0.5 to 0.5)
+ZDISP_INOUT = False # both-direction "b" displacements might be positive or negative; out-only "o" displacements are only positive (out)
 
 def main():
-    vd.refine.refine_directory(PTH_SRC, PTH_DST)
+    vd.refine.refine_directory(PTH_SRC, XYDISP_FULL, ZDISP_INOUT, PTH_DST)
 
 
 
--- vdisp/refine.py
+++ vdisp/refine.py
@@ -4,7 +4,7 @@ from .imgop import apply_cityblock_vignette, apply_gaussian_vignette
 from tqdm import tqdm
 
 # pth_src and pth_dest expect pathlib.Path
-def refine_directory(pth_src, pth_dst = None):
+def refine_directory(pth_src, xydisp_full, zdisp_inout, pth_dst = None):
     if not isinstance(pth_src, pathlib.Path): pth_src = pathlib.Path(pth_src)
     if pth_dst is None: pth_dst = pth_src
     elif not isinstance(pth_dst, pathlib.Path): pth_dst = pathlib.Path(pth_dst)
@@ -16,23 +16,23 @@ def refine_directory(pth_src, pth_dst = None):
         #start_time = time.time()
         pbar.set_description("refining {}".format(f))
         #print("{}".format(f.name))
-        _refine_path(f,pth_dst)
+        _refine_path(f,pth_dst,xydisp_full, zdisp_inout)
         #print("processed {} in {}s".format(f.name, (time.time() - start_time)))
 
 
-def _refine_path(pth_src, pth_dst):
+def _refine_path(pth_src, pth_dst, xydisp_full, zdisp_inout):
     #fname, ext = os.path.splitext(pth_src)
     #print("processing {}".format(pth_src.name))
 
-    img = read_tif16(pth_src) 
+    img = read_tif16(pth_src,xydisp_full, zdisp_inout) 
 
     apply_cityblock_vignette(img,do_save_mask=False)
     #apply_gaussian_vignette(img,do_save_mask=False)
 
     if pth_src.parents[0] == pth_dst:
-        write_tif16(img, pth_src.parents[0] / (pth_src.stem + "_refined.tif") )
+        write_tif16(img, pth_src.parents[0] / (pth_src.stem + "_refined.tif"), xydisp_full, zdisp_inout )
     else:
-        write_tif16(img, pth_dst / pth_src.name )
+        write_tif16(img, pth_dst / pth_src.name, xydisp_full, zdisp_inout )
 
 
 
